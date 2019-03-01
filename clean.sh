cbc2dbc5b123133539a753efa36cf653ffbd82c6
mlabio <upwork.mlabio@gmail.com>
2019-03-01 10:19:04 +0800

    rewrite .gitlab-ci.yml

---
+++ clean.sh
@@ -0,0 +1,1 @@
+docker ps -a | grep Exit | cut -d ' ' -f 1 | xargs --no-run-if-empty docker rm
---
+++ run.sh
@@ -0,0 +1,1 @@
+docker ps -a | awk '{ print $1,$2 }' | grep dockertest | awk '{print $1 }' | xargs -I {} docker rm --force {}
