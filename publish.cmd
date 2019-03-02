f7d89035a16334771ab66f94fcfa960cc6043b94
mlabio <upwork.mlabio@gmail.com>
2019-03-03 01:31:12 +0800

    update

--- publish.cmd
+++ publish.cmd
@@ -2,8 +2,8 @@
 cls
 
 set APP="BPOSolution"
-set DIST_PATH="40.65.191.64 cd \C:\inetpub\wwwroot\dummy\BackEnd"
-set FULL_PATH="40.65.191.64 cd \C:\inetpub\wwwroot\dummy\BackEnd"
+set DIST_PATH="40.65.191.64 cd /C:/inetpub/wwwroot/dummy/"
+set FULL_PATH="40.65.191.64 cd /C:/inetpub/wwwroot/dummy/BackEnd"
 
 IF EXIST %DIST_PATH% (
     sc stop %APP%
